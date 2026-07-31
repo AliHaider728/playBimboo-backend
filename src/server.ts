fb5d1a501b77b483962f591f2eef8c66e42b03bd
AliHaider728 <decantboy67@gmail.com>
2026-07-31 15:10:38 +0500

    "updates"

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:3000',
   'http://localhost:5173',
-  'http://127.0.0.1:3000'
+  'https://play-bimboo.vercel.app'
+  
 ].filter(Boolean) as string[];
 
 app.use(cors({
